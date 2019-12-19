c293d2278db3c2a7489f595d14780cdb52aced61
John Foley <jjfoley@smith.edu>
2019-12-18 21:20:31 -0500

    init

---
+++ cexample/__init__.py
@@ -0,0 +1,38 @@
+from .cexample import lib, ffi
+
+def _handle_rust_str(result) -> str:
+    """
+    This method decodes bytes to UTF-8 and makes a new python string object. 
+    It then frees the bytes that Rust allocated correctly.
+    """
+    try:
+        txt = ffi.cast("char*", result)
+        txt = ffi.string(txt).decode("utf-8")
+        return txt
+    finally:
+        lib.free_str(result)
+
+def _handle_c_result(c_result):
+    """
+    This handles the logical-OR struct of the CDataset { error_message, success } 
+    where both the wrapper and the error_message will be freed by the end of this function.
+    The success pointer is returned or an error is raised!
+    """
+    if c_result == ffi.NULL:
+        raise ValueError("CResult should not be NULL")
+    error = None
+    success = None
+    if c_result.error_message != ffi.NULL:
+        error = _handle_rust_str(c_result.error_message)
+    if c_result.success != ffi.NULL:
+        success = c_result.success
+    lib.free_c_result(c_result)
+    if error is not None:
+        raise Exception(error)
+    return success
+
+def operate(op: char, x: int, y: int) -> int:
+    return _handle_c_result(lib.c_operate(op, x, y))
+
+if __name__ == '__main__':
+    assert operate('-', 7, 3) == 4
---
+++ src/lib.rs
@@ -0,0 +1,79 @@
+use libc::{c_char, c_void};
+use std::ffi::CString;
+use std::ptr;
+use std::fmt;
+
+#[repr(C)]
+pub struct CResult {
+    pub error_message: *const c_void,
+    pub success: *const c_void,
+}
+
+impl Default for CResult {
+    fn default() -> Self {
+        CResult {
+            error_message: ptr::null(),
+            success: ptr::null(),
+        }
+    }
+}
+
+/// Internal helper: convert string reference to pointer to be passed to Python/C. Heap allocation.
+fn return_string(output: &str) -> *const c_void {
+    let c_output: CString = CString::new(output).expect("Conversion to CString should succeed!");
+    CString::into_raw(c_output) as *const c_void
+}
+
+fn result_to_c<T, E: fmt::Debug>(rust_result: Result<T, E>) -> *const CResult {
+    let mut c_result = Box::new(CResult::default());
+    match rust_result {
+        Ok(item) => {
+            let output = Box::new(item);
+            c_result.success = Box::into_raw(output) as *const c_void;
+        }
+        Err(e) => {
+            let error_message = format!("{:?}", e);
+            c_result.error_message = return_string(&error_message);
+        }
+    };
+    Box::into_raw(c_result)
+}
+
+#[no_mangle]
+pub extern "C" fn free_str(originally_from_rust: *mut c_void) {
+    let _will_drop: CString = unsafe { CString::from_raw(originally_from_rust as *mut c_char) };
+}
+
+/// Note: not-recursive. Free Error Message Manually!
+#[no_mangle]
+pub extern "C" fn free_c_result(originally_from_rust: *mut CResult) {
+    let _will_drop: Box<CResult> = unsafe { Box::from_raw(originally_from_rust) };
+}
+
+#[no_mangle]
+pub extern "C" fn c_operate(op: char, x: i32, y: i32) -> *const CResult {
+    result_to_c(operate(op, x, y))
+}
+
+fn operate(op: char, x: i32, y: i32) -> Result<i32, String> {
+    Ok(match op {
+        '+' => x + y,
+        '-' => x - y,
+        '*' => x * y,
+        '/' => x / y,
+        '%' => x % y,
+        '^' => x ^ y,
+        _ => Err(format!("Bad Operator: '{}'", op))?
+    })
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::operate;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(operate('+', 2, 2).unwrap(), 4);
+    }
+}
